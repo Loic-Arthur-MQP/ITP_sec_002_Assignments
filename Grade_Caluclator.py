c6c58a545eafa74778ef1bfb51b1e079d91919ea
Loic-Arthur-MQP <loic.ndjionguenzenang@dawsoncollege.qc.ca>
2024-09-27 10:50:20 -0400

    Update Grade_Caluclator.py

--- Grade_Caluclator.py
+++ Grade_Caluclator.py
@@ -1,20 +1,20 @@
-# all grading func(weight * grade / 6 if lab <= 6 else weight )ons
+# all grading functions
 def grading(grade: float, weight: float) -> float:
     # question what if the student has less than 6 completed labs
     if grade == lab or grade == quiz:
-        return weight * int(grade) / 6 if grade <= 6 else weight
+        return weight * int(grade) / 6 if int(grade) <= 6 else weight
     else:
         return grade * weight * 10 ** -2
 
 
 def tot_grade() -> float:
-    # questions : 1) can we have a grade more than 100 ? 2) what is the user inputs a string ?
     return lab_wgr + quiz_wgr + asgn_wgr + midterms + final_wgr + mid_final_prep_wgr
 
 
 # inputting grades 
-lab = float(input('Enter the number of labs completed: '))
-quiz = float(input('Enter the number of quizzes completed: '))
+lab = input('Enter the number of labs completed: ')
+quiz = input('Enter the number of quizzes completed: ')
+
 print('Enter your grade for the following assessments')
 asgn_1 = float(input('Assignment 1: '))
 asgn_2 = float(input('Assignment 2: '))
@@ -35,4 +35,4 @@ midterms = grading(average_midterms, 25)
 mid_final_prep_wgr = grading(mid_and_final, 6)
 final_wgr = grading(Final, 18)
 
-print(' Your final grade : ', :tot_grade())
+print('Your final grade for the course : ', str(tot_grade())[:6], '%')
