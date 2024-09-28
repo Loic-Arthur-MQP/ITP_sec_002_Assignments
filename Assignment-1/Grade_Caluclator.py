f4d17448ecfd61fb3691c3a8a89444c77b837832
Loic-Arthur-MQP <loic.ndjionguenzenang@dawsoncollege.qc.ca>
2024-09-28 15:52:46 -0400

    Update Grade_Caluclator.py

--- Assignment-1/Grade_Caluclator.py
+++ Assignment-1/Grade_Caluclator.py
@@ -1,22 +1,21 @@
 # all grading functions
-def grading(grade: float, weight: float) -> float:
-    # question what if the student has less than 6 completed labs
+def grading(grade, weight: float) -> float:
     if grade == lab or grade == quiz:
         return weight * int(grade) / 6 if int(grade) <= 6 else weight
     else:
         return grade * weight * 10 ** -2
 
 
-def tot_grade() -> float:
+def total() -> float:
     return lab_wgr + quiz_wgr + asgn_wgr + midterms + final_wgr + mid_final_prep_wgr
 
 
-# inputting grades 
-
+# inputting grades (asgn = assignment)
+print('Enter only numerical values.')
 lab: str = input('Enter the number of labs completed: ')
 quiz: str = input('Enter the number of quizzes completed: ')
-
-print('Enter your grade for the following assessments')
+print('
+Enter your grades for the following assessments.')
 asgn_1: float = float(input('Assignment 1: '))
 asgn_2: float = float(input('Assignment 2: '))
 asgn_3: float = float(input('Assignment 3: '))
@@ -26,7 +25,7 @@ midterm2: float = float(input('Midterm 2: '))
 Final: float = float(input('Final Exam: '))
 mid_and_final: float = float(input('Midterms + Final Preparation: '))
 
-# computing total grades (wgr = weighted grade, asgn = assignment)
+# computing total grades (wgr = weighted grade)
 lab_wgr = grading(lab, 20)
 quiz_wgr = grading(quiz, 15)
 average_asgn = (asgn_1 + asgn_2 + asgn_3 + asgn_4) / 4
@@ -36,4 +35,8 @@ midterms = grading(average_midterms, 25)
 mid_final_prep_wgr = grading(mid_and_final, 6)
 final_wgr = grading(Final, 18)
 
-print('Your final grade for the course : ', str(tot_grade())[:6], '%')
+tot_grade = str(total())
+second_decimal = tot_grade[5:6]
+desired_digits = tot_grade[:5]
+rounded_tot_grade = float(desired_digits) + 0.01 if second_decimal >= 5 else float(desired_digits) 
+print('Your final grade for the course : ', rounded_tot_grade, '%')
