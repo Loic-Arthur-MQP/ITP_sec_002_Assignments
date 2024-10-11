c5e3506f0d49e3db0f31c8c46237db242634df9d
loicn <l@l>
2024-10-10 21:23:01 -0400

    update

--- assignment-2/peace.py
+++ assignment-2/peace.py
@@ -8,46 +8,70 @@ suits = ("hearts", "diamonds", "clubs", "spades")
 # Create a deck of cards
 deck = [(suit, rank) for suit in suits for rank in ranks]
 
-# Shuffle the deck 
+# Shuffle the deck
 random.shuffle(deck)
 
 # Split the deck into two hands
-p1_deck = deck[:len(deck)//+1]
-p2_deck = deck[len(deck)//+1:]
+p1_deck = deck[:len(deck) // 2]
+p2_deck = deck[len(deck) // 2:]
 
 
 def card_comparison(p1_card: tuple, p2_card: tuple):
     def specify_index(x: str):
         return ranks.index(x)
 
-    higher_rank = max(p1_card[1],p2_card[2], key=specify_index)
-
-    if higher_rank == p1_card[1]:
-        return 1
-    elif higher_rank == p2_card[2]:
-        return 2
-    elif p1_card[1] == p2_card:
+    # noinspection PyTypeChecker
+    higher_rank = max(p1_card[1], p2_card[1], key=specify_index)
+    if p1_card[1] == p2_card[1]:
         return 0
+    elif higher_rank == p1_card[1]:
+        return 1
+    elif higher_rank == p2_card[1]:
+        return 2
 
 
-def play_round(player1_hand, player2_hand):
-    """Play a single round of the game.
-		That is, each player flips a card, and the winner is determined using the card_comparison function
-		if both players flip the same value card, call the war function
-	"""
-    # Your code here
+def play_round(player1_hand: list, player2_hand: list):
+    p1_card = player1_hand.pop(0)
+    p2_card = player2_hand.pop(0)
+    result = card_comparison(p1_card, p2_card)
+    if result == 1:
+        p1_deck.extend([p1_card, p2_card])
+    elif result == 2:
+        p2_deck.extend([p1_card, p2_card])
+    else:
+        print('WAR !!!!!')
+        if len(player1_hand) >= 4 and len(player2_hand) >= 4:
+            war(player1_hand, player2_hand, p1_card, p2_card)
+        else:
+            return 'Player 1 won' if len(p1_deck) > len(p2_deck) else 'Player 2 won'
 
-def war(player1_hand, player2_hand):
-    """Handle the 'war' scenario when cards are equal.
-		recall the rules of war, both players put 3 cards face down, 
-		then both players flip face up a 4th card. The player with the stronger
-		card takes all the cards.		
-	"""
-    # Your code here
+    return result
+
+
+def war(player1_hand, player2_hand, p1_card, p2_card):
+    p1_triplet = [p1_card]
+    p2_triplet = [p2_card]
+    for num in range(4):
+        p1_triplet.append(player1_hand.pop(0))
+        p2_triplet.append(player2_hand.pop(0))
+
+    result = card_comparison(p1_triplet[-1], p2_triplet[-1])
+    if result == 0:
+        war(player1_hand,player2_hand, p1_triplet[-1], p2_triplet[-1])
+    elif result == 1:
+        p1_deck.extend(p1_triplet+p2_triplet)
+    elif result == 2:
+        p2_deck.extend(p1_triplet+p2_triplet)
+
+    return result
+
 
 def play_game():
-    """Main function to run the game."""
-    # Your code here
+    result = 0
+    while p1_deck != [] and p2_deck != []:
+        result = play_round(p1_deck, p2_deck)
+    print(len(p1_deck), len(p2_deck))
+    return 'Player 1 won' if p1_deck != [] else 'Player 2 won'
 
-# Call the main function to start the game
-play_game()
+
+print(play_game())
