722950cc300b6ec644a01c42cb4c42612ec40de0
loicn <l@l>
2024-10-12 13:24:57 -0400

    Finished project

--- project_not_done/Assignment-2/peace.py
+++ project_not_done/Assignment-2/peace.py
@@ -1,8 +1,11 @@
 # Import necessary modules
 import random
 
+modifiers = ['One more', 'Another', 'Again!!!', 'Infinite', 'Brutal', 'Non stop', 'An extra', 'A further', 'World',
+             'Not Peaceful', 'Continuous', 'Unbreakable', '1000']
+
 # Define the ranks and suits
-ranks = ("2", "3", "4")
+ranks = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
 suits = ("hearts", "diamonds", "clubs", "spades")
 
 # Create a deck of cards
@@ -14,15 +17,14 @@ random.shuffle(deck)
 # Split the deck into two hands
 p1_deck = deck[:len(deck) // 2]
 p2_deck = deck[len(deck) // 2:]
-state = 1
+p1_quintuplets = []
+p2_quintuplets = []
+war_chain = [1]
 
 
 def card_comparison(p1_card: tuple, p2_card: tuple):
-    def specify_index(x: str):
-        return ranks.index(x)
-
     # noinspection PyTypeChecker
-    higher_rank = max(p1_card[1], p2_card[1], key=specify_index)
+    higher_rank = max(p1_card[1], p2_card[1], key=ranks.index)
     if p1_card[1] == p2_card[1]:
         return 0
     elif higher_rank == p1_card[1]:
@@ -41,51 +43,58 @@ def play_round(player1_hand: list, player2_hand: list):
     elif result == 2:
         p2_deck.extend([p1_card, p2_card])
     else:
-        print('WAR !!!!!')
-        if len(player1_hand) >= 4 and len(player2_hand) >= 4:
-            war(player1_hand, player2_hand, p1_card, p2_card)
-        else:
-            return 'Player 1 won' if len(p1_deck) > len(p2_deck) else 'Player 2 won'
-    print(f'play\'s {len(p1_deck)}, {len(p2_deck)}')
-    print(f'player {result} won the round')
-    print('done')
+        print('WAR !!!!')
+        result = war(player1_hand, player2_hand, p1_card, p2_card, p1_quintuplets, p2_quintuplets)
     return result
 
 
-def war(player1_hand, player2_hand, p1_card, p2_card):
-    global state
-    print('war fare')
-    p1_triplet = [p1_card]
-    p2_triplet = [p2_card]
+def war(player1_hand, player2_hand, p1_card, p2_card, war_deck1, war_deck2):
+    if war_chain:
+        war_deck1.append(p1_card)
+        war_deck2.append(p2_card)
+    else:
+        war_chain.append(1)
+
+    print(f'tri 1 = {len(war_deck1)}, tri 2 = {len(war_deck2)}, {war_deck1==p1_quintuplets}{war_deck2==p2_quintuplets}')
     if len(player1_hand) >= 4 and len(player2_hand) >= 4:
         for num in range(4):
-            p1_triplet.append(player1_hand.pop(0))
-            p2_triplet.append(player2_hand.pop(0))
+            war_deck1.append(player1_hand.pop(0))
+            war_deck1.append(player2_hand.pop(0))
     else:
-        print('changing state')
         print(f'War\'s {len(p1_deck)}, {len(p2_deck)}')
-        print('Player 1 won the game' if len(p1_deck) > len(p2_deck) else 'Player 2 won the game')
-        state = 0
-        return 0
+        rest1 = [p1_deck.pop(0) for x in range(len(p1_deck))]
+        rest2 = [p2_deck.pop(0) for x in range(len(p2_deck))]
 
-    result = card_comparison(p1_triplet[-1], p2_triplet[-1])
+        if len(rest1) == len(rest2):
+            return 0
+        return 1 if len(rest1) > len(rest2) else 2
+
+    result = card_comparison(war_deck1[-1], war_deck2[-1])
     if result == 0:
-        print('WAR !!!!!!!')
-        war(player1_hand, player2_hand, p1_triplet[-1], p2_triplet[-1])
+        war_chain.remove(1)
+        print(f'{random.choice(modifiers)} WAR !!!!')
+        war(player1_hand, player2_hand, war_deck1[-1], war_deck2[-1], p1_quintuplets, p2_quintuplets)
     elif result == 1:
-        p1_deck.extend(p2_triplet + p1_triplet)
+        p1_deck.extend(war_deck2 + war_deck1)
+        war_deck1.clear()
+        war_deck2.clear()
     elif result == 2:
-        p2_deck.extend(p1_triplet + p2_triplet)
+        p2_deck.extend(war_deck1 + war_deck2)
+        war_deck1.clear()
+        war_deck2.clear()
 
-    return 1
+    return result
 
 
 def play_game():
-    while p1_deck != [] and p2_deck != [] and state:
-        play_round(p1_deck, p2_deck)
-    print(f'state {state}')
+    result = 0
+    while p1_deck != [] and p2_deck != []:
+        result = play_round(p1_deck, p2_deck)
     print(len(p1_deck), len(p2_deck))
-    return 'Player 1 won the game' if p1_deck != [] else 'Player 2 won the game'
+    if not result:
+        return 'WHAT A DRAW !'
+    else:
+        return f'Player {result} won the game'
 
 
 print(play_game())
