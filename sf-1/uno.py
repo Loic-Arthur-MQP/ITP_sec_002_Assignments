a93f8adc9543647e2a3a3e8ddcd0f2ed6107a1cf
loicn <l@l>
2024-10-17 00:03:37 -0400

    improving uno

--- sf-1/uno.py
+++ sf-1/uno.py
@@ -1,5 +1,5 @@
 import random
-
+import time
 
 colours = ('red', 'blue', 'green', 'yellow', 'wild')
 ranks = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+2', 'skip', 'reverse')
@@ -29,20 +29,43 @@ com_deck = [deck.pop(0) for _ in range(7)]
 whose_turn = random.choice([1,2])
 
 def computer_play(com_hand ,central_card):
+    
 
 def valid_play(p_hand, central_card):
+    time.sleep(1)
     print(p_hand)
     play_decision = input('Play (1) or Draw (0) : ')
     if play_decision:
+        time.sleep(0.5)
         p_choice = int(input('Choose which card to play : ')) - 1
+        p_card = p_hand[p_choice]
+        if p_card in special_cards:
+            action_cards1(p_card) # calling function to implement the action card
+
+        elif p_card in power_cards:
+            action_cards2(p_card)
+        elif p_card[0] == central_card[0] or p_card[1] == central_card[1]:
+            discard_pile.append(p_card)
+        else:
+            print("You can't play that card! 2 cards penalty")
+            time.sleep(0.5)
+            p_hand.append(deck.pop(0))
+            p_hand.append(deck.pop(0))
+            whose_turn = 2
     else:
         p_hand.append(deck.pop(0))
         whose_turn = 2
-    if p_hand[0] == central_card[0] or p_hand[1] == central_card[1]:
 
 
+def action_cards1(card): # implementing Action cards for special cards
+    return
 
-        print(p_hand)
+def action_cards2(card):  # implementing Action cards for power_cards
+    return
 
-
-    else:
+def main_game_play():
+    while p_deck != [] and com_deck != []:
+        while whose_turn == 1:
+            valid_play()
+        while whose_turn == 2:
+            computer_play()
