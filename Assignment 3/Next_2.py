f169ec2e910c8a01b5e2973f6a9e2985ae774ce3
loicn <l@l>
2024-11-01 20:15:26 -0400

    solution for next-2

--- Assignment 3/Next_2.py
+++ Assignment 3/Next_2.py
@@ -1,3 +1,16 @@
+''' 
+Start iterating from the second element (index 1) to the end of the list.
+
+For each element:
+
+If the element is odd, replace it with the previous element plus 2.
+
+If the element is even, calculate the average of all previous elements, floor the average to the nearest even number, and replace the current element with this value.
+
+Return the modified list.
+'''
+
+
 def next_2(nums) -> list:
     for i in range(1, len(nums)):
         if nums[i] % 2 != 0:
